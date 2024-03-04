541f9fa4dac78710899ee3241e95d7e5d07078bf
Bobiktrans <Bobicktrnas@yandex.ru>
2024-03-05 00:12:12 +0300

    xcvxcv
    Signed-off-by: Bobiktrans <Bobicktrnas@yandex.ru>

---
+++ src/main/java/com/example/Bank/Controler/ControllerBank.java
@@ -0,0 +1,13 @@
+package com.example.Bank.Controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerBank {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/Bank/Entity/Client.java
@@ -0,0 +1,38 @@
+package com.example.Bank.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
